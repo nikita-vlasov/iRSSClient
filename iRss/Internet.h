204a4dbf47f4c3730a57136852b26f80b3ebf600
Alex <alex.kovalskiy@icloud.com>
2013-12-08 01:29:17 +0200

    Добавлен класс для проверки на интернет соединение.

---
+++ iRss/Internet.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+@interface Internet : NSObject {
+    
+}
+
+#pragma mark -
++ (BOOL)internetConnection;
+
+@end
---
+++ iRss/Internet.m
@@ -0,0 +1,5 @@
+#import "Internet.h"
+
+@implementation Internet
+
+@end
