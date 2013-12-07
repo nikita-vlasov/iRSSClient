1e308c5b3da36741bdd774e287cd2cf05476140a
Alex <alex.kovalskiy@icloud.com>
2013-12-08 01:22:25 +0200

    Initial Commit

---
+++ iRss/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iRss
+//
+//  Created by Alex on 12/8/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ iRssTests/iRssTests.m
@@ -0,0 +1,34 @@
+//
+//  iRssTests.m
+//  iRssTests
+//
+//  Created by Alex on 12/8/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iRssTests : XCTestCase
+
+@end
+
+@implementation iRssTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
