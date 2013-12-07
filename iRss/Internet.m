7c759735fa6ddab46ec699a149ed8c676f81d576
Alex <alex.kovalskiy@icloud.com>
2013-12-08 01:33:02 +0200

    Добавлен метод для проверки интернет соединение. Создан класс для роботы с таблицей.

--- iRss/Internet.m
+++ iRss/Internet.m
@@ -2,4 +2,15 @@
 
 @implementation Internet
 
++ (BOOL)internetConnection {
+    NSURL *url = [NSURL URLWithString:@"http://www.google.com/"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"HEAD"];
+    NSHTTPURLResponse *response;
+    
+    [NSURLConnection sendSynchronousRequest:request returningResponse:&response error: NULL];
+    
+    return ([response statusCode] == 200) ? YES : NO;
+}
+
 @end
