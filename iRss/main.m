755cd5d058928864a7b113d82a24c7c5715d59b0
Alex <alex.kovalskiy@icloud.com>
2013-12-08 04:05:44 +0200

    Push на bitbucket.

--- iRss/main.m
+++ iRss/main.m
@@ -1,17 +1,7 @@
-//
-//  main.m
-//  iRss
-//
-//  Created by Alex on 12/8/13.
-//  Copyright (c) 2013 Alex. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
-
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
