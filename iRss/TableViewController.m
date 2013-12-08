535cec4e2480e781355a5ad7aa0000d89a5ac819
Alex <alex.kovalskiy@icloud.com>
2013-12-08 02:13:33 +0200

    Реализован интерфейс детального отображения новостей. Добавлены изображения социальных сетей.

--- iRss/TableViewController.m
+++ iRss/TableViewController.m
@@ -71,9 +71,6 @@
 
 #pragma mark - Navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
-
 
 @end
