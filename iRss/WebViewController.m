f080078be2c2847f82d0a0ffe5f9a34bd7da063f
Alex <alex.kovalskiy@icloud.com>
2013-12-08 02:43:10 +0200

    Добавлен класс для WebView.

---
+++ iRss/WebViewController.m
@@ -0,0 +1,26 @@
+#import "WebViewController.h"
+
+@interface WebViewController () {
+    
+}
+
+@end
+
+
+@implementation WebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
