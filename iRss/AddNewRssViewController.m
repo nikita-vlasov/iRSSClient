581b551e7b89e4dd9e3a3f4062abba26b2d71bd7
Alex <alex.kovalskiy@icloud.com>
2013-12-11 00:59:58 +0200

    Добавлен контролер для добавления RSS каналов.

---
+++ iRss/AddNewRssViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddNewRssViewController.m
+//  iRss
+//
+//  Created by Alex on 12/11/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import "AddNewRssViewController.h"
+
+@interface AddNewRssViewController ()
+
+@end
+
+@implementation AddNewRssViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
