a0afa0e4cf6df6d04ee6c3b26362d2f5e1763c27
Alex <alex.kovalskiy@icloud.com>
2013-12-11 01:00:53 +0200

    Убраны лишнее комментарии.

--- iRss/AddNewRssViewController.h
+++ iRss/AddNewRssViewController.h
@@ -1,13 +1,8 @@
-//
-//  AddNewRssViewController.h
-//  iRss
-//
-//  Created by Alex on 12/11/13.
-//  Copyright (c) 2013 Alex. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-@interface AddNewRssViewController : UIViewController
+@interface AddNewRssViewController : UIViewController {
+    
+}
+
 
 @end
--- iRss/AddNewRssViewController.m
+++ iRss/AddNewRssViewController.m
@@ -1,38 +1,26 @@
-//
-//  AddNewRssViewController.m
-//  iRss
-//
-//  Created by Alex on 12/11/13.
-//  Copyright (c) 2013 Alex. All rights reserved.
-//
-
 #import "AddNewRssViewController.h"
 
-@interface AddNewRssViewController ()
+@interface AddNewRssViewController () {
+    
+}
 
 @end
 
+
 @implementation AddNewRssViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
     }
     return self;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
