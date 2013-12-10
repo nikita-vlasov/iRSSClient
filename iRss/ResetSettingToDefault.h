305732f189fa343536d2c400f3a8f8e80c7f64e2
Alex <alex.kovalskiy@icloud.com>
2013-12-10 07:40:24 +0200

    Добавлен класс для сброса пользовательский настроек.

---
+++ iRss/ResetSettingToDefault.h
@@ -0,0 +1,13 @@
+//
+//  ResetSettingToDefault.h
+//  iRss
+//
+//  Created by Alex on 12/10/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ResetSettingToDefault : NSObject
+
+@end
---
+++ iRss/ResetSettingToDefault.m
@@ -0,0 +1,13 @@
+//
+//  ResetSettingToDefault.m
+//  iRss
+//
+//  Created by Alex on 12/10/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import "ResetSettingToDefault.h"
+
+@implementation ResetSettingToDefault
+
+@end
