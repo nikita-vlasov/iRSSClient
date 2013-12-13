406fa28c85fa86323b0cc0ec0668b4e41b8f3bf3
Alex <alex.kovalskiy@icloud.com>
2013-12-13 22:52:35 +0200

    Убраны лишнее переменные.

--- iRss/DetailSettingTableViewController.h
+++ iRss/DetailSettingTableViewController.h
@@ -8,13 +8,8 @@
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 
 #pragma mark -
-@property (strong, nonatomic) NSString *stringCellLabel;
-@property (strong, nonatomic) NSString *stringCellDetailLabel;
 @property (strong, nonatomic) NSString *stringTitleForFooterSection;
-
-#pragma mark -
 @property (nonatomic) NSInteger countNumberRows;
-
 @property (strong, nonatomic) NSArray *arrayTitle;
 @property (strong, nonatomic) NSArray *arrayDescription;
 
