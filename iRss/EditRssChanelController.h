2570f90f6d52fb8fffa7e481ba302d325e508672
Alex <alex.kovalskiy@icloud.com>
2013-12-14 16:17:27 +0200

    Добавлен клас для работы с редактированием RSS канала.

---
+++ iRss/EditRssChanelController.h
@@ -0,0 +1,13 @@
+//
+//  EditRssChanelController.h
+//  iRss
+//
+//  Created by Alex on 12/14/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditRssChanelController : UITableViewController
+
+@end
