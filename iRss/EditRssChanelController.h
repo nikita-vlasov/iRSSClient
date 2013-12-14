7204ec612678ff3b7091507741d4b764fd51270d
Alex <alex.kovalskiy@icloud.com>
2013-12-14 16:19:51 +0200

    С класса убраны лишние комментарии.

--- iRss/EditRssChanelController.h
+++ iRss/EditRssChanelController.h
@@ -1,13 +1,7 @@
-//
-//  EditRssChanelController.h
-//  iRss
-//
-//  Created by Alex on 12/14/13.
-//  Copyright (c) 2013 Alex. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-@interface EditRssChanelController : UITableViewController
+@interface EditRssChanelController : UITableViewController <UITableViewDataSource, UITableViewDelegate> {
+    
+}
 
 @end
--- iRss/EditRssChanelController.m
+++ iRss/EditRssChanelController.m
@@ -1,67 +1,42 @@
-//
-//  EditRssChanelController.m
-//  iRss
-//
-//  Created by Alex on 12/14/13.
-//  Copyright (c) 2013 Alex. All rights reserved.
-//
-
 #import "EditRssChanelController.h"
 
-@interface EditRssChanelController ()
+@interface EditRssChanelController () {
+
+}
 
 @end
 
+
 @implementation EditRssChanelController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
+- (id)initWithStyle:(UITableViewStyle)style {
     self = [super initWithStyle:style];
     if (self) {
-        // Custom initialization
     }
     return self;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 3;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
     
-    // Configure the cell...
+    
     
     return cell;
 }
