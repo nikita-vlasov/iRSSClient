8a7d54a013cce950ef04ecdcafd03c57add171e8
Alex <alex.kovalskiy@icloud.com>
2013-12-14 16:21:00 +0200

    Добавлен класс для кастомных ячеек.

---
+++ iRss/CellEditChanel.h
@@ -0,0 +1,13 @@
+//
+//  CellEditChanel.h
+//  iRss
+//
+//  Created by Alex on 12/14/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CellEditChanel : UITableViewCell
+
+@end
---
+++ iRss/CellEditChanel.m
@@ -0,0 +1,28 @@
+//
+//  CellEditChanel.m
+//  iRss
+//
+//  Created by Alex on 12/14/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import "CellEditChanel.h"
+
+@implementation CellEditChanel
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
