b36724c7742921d9907de89ef2435922c91b3395
Alex <alex.kovalskiy@icloud.com>
2013-12-16 02:40:34 +0200

    Добавлены переменные для хранения данных.

--- iRss/EditChannelTableViewController.h
+++ iRss/EditChannelTableViewController.h
@@ -1,7 +1,10 @@
 #import <UIKit/UIKit.h>
 
 @interface EditChannelTableViewController : UITableViewController <UITableViewDataSource, UITabBarDelegate> {
-    
+    @private
+    NSString *stringTitle;
+    NSString *stringLink;
+    NSString *stringDescription;    
 }
 
 #pragma mark - Outlet
