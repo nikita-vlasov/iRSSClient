5a98854dda444b318a389588ac90191992eb7e1b
Alex <alex.kovalskiy@icloud.com>
2013-12-17 22:55:41 +0200

    Добавлен клас и контролер для вывода на экран только текста.

---
+++ iRss/OneTextView.m
@@ -0,0 +1,38 @@
+//
+//  OneTextView.m
+//  iRss
+//
+//  Created by Alex on 12/17/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import "OneTextView.h"
+
+@interface OneTextView ()
+
+@end
+
+@implementation OneTextView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
