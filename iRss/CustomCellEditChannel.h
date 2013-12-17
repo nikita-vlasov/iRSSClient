b96d4d0a90292aeb890363559f259656071d33f9
Alex <alex.kovalskiy@icloud.com>
2013-12-18 01:24:55 +0200

    Удален label на ячейке в контролере добавления/редактирования канала.

--- iRss/CustomCellEditChannel.h
+++ iRss/CustomCellEditChannel.h
@@ -5,7 +5,6 @@
 }
 
 #pragma mark - Outlet
-@property (weak, nonatomic) IBOutlet UILabel *labelCellEdit;
 @property (weak, nonatomic) IBOutlet UITextField *textFieldCellEdit;
 @property (weak, nonatomic) IBOutlet UIImageView *imageViewCell;
 
