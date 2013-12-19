c7683e056c5883f6bfc8b53d950b07f80974675a
Alex <alex.kovalskiy@icloud.com>
2013-12-19 21:23:02 +0200

    Добавлен класс для того, что бы вынести все запросы к базе данных.

--- iRss/Client.h
+++ iRss/Client.h
@@ -1,13 +1,7 @@
-//
-//  Client.h
-//  iRss
-//
-//  Created by Alex on 12/19/13.
-//  Copyright (c) 2013 Alex. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
-@interface Client : NSObject
+@interface Client : NSObject {
+    
+}
 
 @end
--- iRss/Client.m
+++ iRss/Client.m
@@ -1,11 +1,3 @@
-//
-//  Client.m
-//  iRss
-//
-//  Created by Alex on 12/19/13.
-//  Copyright (c) 2013 Alex. All rights reserved.
-//
-
 #import "Client.h"
 
 @implementation Client
