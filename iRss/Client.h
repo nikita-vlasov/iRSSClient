fa6d865482842a33af71ce8f9d8ef655f9697d7c
Alex <alex.kovalskiy@icloud.com>
2013-12-19 14:23:14 +0200

    Добавлен класс для роботы с БД.

---
+++ iRss/Client.h
@@ -0,0 +1,13 @@
+//
+//  Client.h
+//  iRss
+//
+//  Created by Alex on 12/19/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Client : NSObject
+
+@end
---
+++ iRss/Client.m
@@ -0,0 +1,13 @@
+//
+//  Client.m
+//  iRss
+//
+//  Created by Alex on 12/19/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import "Client.h"
+
+@implementation Client
+
+@end
