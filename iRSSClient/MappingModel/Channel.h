e3ae7883a9ed6dc9164c10a813f6a97f9d336f47
Alex <alex.kovalskiy@icloud.com>
2014-03-21 22:35:51 +0200

    Добавлен класс для мапинга.

---
+++ iRSSClient/MappingModel/Channel.h
@@ -0,0 +1,13 @@
+//
+//  Channel.h
+//  iRSSClient
+//
+//  Created by Alex on 3/21/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Channel : NSObject
+
+@end
---
+++ iRSSClient/MappingModel/Channel.m
@@ -0,0 +1,13 @@
+//
+//  Channel.m
+//  iRSSClient
+//
+//  Created by Alex on 3/21/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "Channel.h"
+
+@implementation Channel
+
+@end
