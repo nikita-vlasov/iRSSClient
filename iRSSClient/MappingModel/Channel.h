aa9928c5e84c3ca74680c959149c1fd336516a05
Alex <alex.kovalskiy@icloud.com>
2014-03-21 22:45:11 +0200

    Переименовано поле (переменная).

--- iRSSClient/MappingModel/Channel.h
+++ iRSSClient/MappingModel/Channel.h
@@ -1,10 +1,11 @@
 #import <Foundation/Foundation.h>
 
 @interface Channel : NSObject
+
 /** Заголовок канала. */
 @property (strong, nonatomic) NSString *title;
 /** Описание канала. */
-@property (strong, nonatomic) NSString *description;
+@property (strong, nonatomic) NSString *designation;
 /** Ссылка на канал. */
 @property (strong, nonatomic) NSString *link;
 
