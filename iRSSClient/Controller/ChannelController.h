3fbe64d5c3e4d5d87ae82ae9a0f7b3eb3044d926
Alex <alex.kovalskiy@icloud.com>
2014-03-21 15:18:19 +0200

    Добавлен вид/контролер для реализации списка каналов.

---
+++ iRSSClient/Controller/ChannelController.h
@@ -0,0 +1,13 @@
+//
+//  ChannelController.h
+//  iRSSClient
+//
+//  Created by Alex on 3/21/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChannelController : UIViewController
+
+@end
---
+++ iRSSClient/View/ChannelView.h
@@ -0,0 +1,13 @@
+//
+//  ChannelView.h
+//  iRSSClient
+//
+//  Created by Alex on 3/21/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChannelView : UIView
+
+@end
