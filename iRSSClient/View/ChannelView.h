2d2aded7fb67f00cc2ee7020c18e99bcf1fd5b0e
Alex <alex.kovalskiy@icloud.com>
2014-03-21 15:45:15 +0200

    Объявлены элементы интерфейса.

--- iRSSClient/View/ChannelView.h
+++ iRSSClient/View/ChannelView.h
@@ -2,4 +2,11 @@
 
 @interface ChannelView : UIView
 
+#pragma mark - UIBarButtonItem
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *leftBarButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *rightBarButton;
+
+#pragma mark - UITableView
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
 @end
