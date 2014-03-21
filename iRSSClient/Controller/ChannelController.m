0fea1afc22c26208496bf20cbb1af92c8a95c899
Alex <alex.kovalskiy@icloud.com>
2014-03-21 15:45:54 +0200

    Реализованы необходимые методы для реализации списка каналов.

--- iRSSClient/Controller/ChannelController.m
+++ iRSSClient/Controller/ChannelController.m
@@ -1,7 +1,10 @@
 #import "ChannelController.h"
 #import "ChannelView.h"
 
-@interface ChannelController ()
+@interface ChannelController () <UITableViewDataSource, UITableViewDelegate> {
+    @private
+    NSMutableArray *arrayChannel;
+}
 
 #pragma mark - UIView
 @property (strong, nonatomic) IBOutlet ChannelView *channelView;
@@ -14,10 +17,40 @@
 #pragma mark - UIViewController
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    [[[self channelView] leftBarButton] setAction:@selector(openFavorites:)];
+    [[[self channelView] rightBarButton] setAction:@selector(openSetting:)];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark - UIBarButtonItem
+- (void)openSetting:(UIBarButtonItem *)sender {
+}
+
+- (void)openFavorites:(UIBarButtonItem *)sender {
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *basicCell = [[[self channelView] tableView] dequeueReusableCellWithIdentifier:@"BasicCell"];
+
+    return basicCell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
