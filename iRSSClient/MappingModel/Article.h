5a2436ee0d1772be84eec7220d38f20a1e975b8d
Alex <alex.kovalskiy@icloud.com>
2014-03-21 22:40:29 +0200

    Добавлен класс для мапинга.

---
+++ iRSSClient/MappingModel/Article.h
@@ -0,0 +1,13 @@
+//
+//  Article.h
+//  iRSSClient
+//
+//  Created by Alex on 3/21/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Article : NSObject
+
+@end
