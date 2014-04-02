28f1051647fc44aad9fccd89e143c43be50e8fc4
Alex <alex.kovalskiy@icloud.com>
2014-04-02 21:13:05 +0300

    + необходимые методы для реализации списка в боковом меню.

--- iRSSClient/Controller/LeftController.m
+++ iRSSClient/Controller/LeftController.m
@@ -1,7 +1,12 @@
 #import "LeftController.h"
 #import "LeftView.h"
+#import "UIViewController+MMDrawerController.h"
+#import "DrawerFactory.h"
 
-@interface LeftController ()
+@interface LeftController () <UITableViewDataSource, UITableViewDelegate> {
+    @private
+    NSMutableArray *arrayTitle;
+}
 
 @property (strong, nonatomic) IBOutlet LeftView *leftView;
 
@@ -13,10 +18,56 @@
 #pragma mark - UIViewCOntroller
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    arrayTitle = [[NSMutableArray alloc] init];
+
+    [arrayTitle addObject:@{@"title" : NSLocalizedString(@"Dashboard", nil), @"icon" : @""}];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [arrayTitle count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *ringDetailCell = [tableView dequeueReusableCellWithIdentifier:@"RingDetailCell"];
+
+    [[ringDetailCell textLabel] setText:[[arrayTitle objectAtIndex:[indexPath row]] valueForKey:@"title"]];
+
+    return ringDetailCell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+//    [self openControllers:[indexPath row]];
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+/** Метод, дает возможность открыть необходимы контролер по индексу ячейки. */
+- (void)openControllers:(int)index {
+//    if (index == 0) {
+//        [[self mm_drawerController] setCenterViewController:[[DrawerFactory sharedFactory] dashboardController] withCloseAnimation:YES completion:nil];
+//    }
+//    else if (index == 1) {
+//        [[self mm_drawerController] setCenterViewController:[[DrawerFactory sharedFactory] customersController] withCloseAnimation:YES completion:nil];
+//    }
+//    else if (index == 2) {
+//        [[self mm_drawerController] setCenterViewController:[[DrawerFactory sharedFactory] vendorsController] withCloseAnimation:YES completion:nil];
+//    }
+//    else if (index == 3) {
+//        [[self mm_drawerController] setCenterViewController:[[DrawerFactory sharedFactory] historyController] withCloseAnimation:YES completion:nil];
+//    }
+//    else if (index == 4) {
+//        [[self mm_drawerController] setCenterViewController:[[DrawerFactory sharedFactory] settingController] withCloseAnimation:YES completion:nil];
+//    }
 }
 
 @end
