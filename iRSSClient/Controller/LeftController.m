c6be1224073ec5d1a9dc403753ad156992748621
Alex <alex.kovalskiy@icloud.com>
2014-04-02 20:54:21 +0300

    add controller class.

---
+++ iRSSClient/Controller/LeftController.m
@@ -0,0 +1,38 @@
+//
+//  LeftController.m
+//  iRSSClient
+//
+//  Created by Alex on 4/2/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "LeftController.h"
+
+@interface LeftController ()
+
+@end
+
+@implementation LeftController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
