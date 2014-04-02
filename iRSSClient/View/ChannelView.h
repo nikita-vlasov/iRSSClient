74837d9c34ba0efe490dc0b686d0fa6e2d4312e1
Alex <alex.kovalskiy@icloud.com>
2014-04-02 22:00:09 +0300

    Класс перенаследован.

--- iRSSClient/View/ChannelView.h
+++ iRSSClient/View/ChannelView.h
@@ -1,13 +1,12 @@
 #import <UIKit/UIKit.h>
+#import "LeftView.h"
 
-@interface ChannelView : UIView
+@interface ChannelView : LeftView
 
 #pragma mark - UIBarButtonItem
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *leftBarButton;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *rightBarButton;
 
-#pragma mark - UITableView
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @property (strong, nonatomic) UIBarButtonItem *barButtonOpenChannel;
 
