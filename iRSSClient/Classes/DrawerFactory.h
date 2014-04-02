5b2b61b2c8d425f5a8f54c9d9b35d8d2ef7f3ed7
Alex <alex.kovalskiy@icloud.com>
2014-04-02 20:51:44 +0300

    Удалены комментарии.

--- iRSSClient/Classes/DrawerFactory.h
+++ iRSSClient/Classes/DrawerFactory.h
@@ -1,11 +1,3 @@
-//
-//  DrawerFactory.h
-//  iRSSClient
-//
-//  Created by Alex on 4/2/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface DrawerFactory : NSObject
--- iRSSClient/Classes/DrawerFactory.m
+++ iRSSClient/Classes/DrawerFactory.m
@@ -1,11 +1,3 @@
-//
-//  DrawerFactory.m
-//  iRSSClient
-//
-//  Created by Alex on 4/2/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import "DrawerFactory.h"
 
 @implementation DrawerFactory
