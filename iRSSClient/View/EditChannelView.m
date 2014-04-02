7d51fc77643ab82f7e400dc3ea04a9947dd52640
Alex <alex.kovalskiy@icloud.com>
2014-04-02 23:18:41 +0300

    Удалены комментарии.

--- iRSSClient/View/EditChannelView.m
+++ iRSSClient/View/EditChannelView.m
@@ -1,21 +1,10 @@
-//
-//  EditChannelView.m
-//  iRSSClient
-//
-//  Created by Alex on 4/2/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import "EditChannelView.h"
 
 @implementation EditChannelView
 
-- (id)initWithFrame:(CGRect)frame
-{
+- (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
+    if (self) {}
     return self;
 }
 
