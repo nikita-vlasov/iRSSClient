1dfe4b48ca906b4ab58af368e8b69891e763e956
Alex <alex.kovalskiy@icloud.com>
2014-04-02 23:39:50 +0300

    add class RssFeedVew.

---
+++ iRSSClient/View/RssFeedView.m
@@ -0,0 +1,31 @@
+//
+//  RssFeedView.m
+//  iRSSClient
+//
+//  Created by Alex on 4/2/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "RssFeedView.h"
+
+@implementation RssFeedView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
