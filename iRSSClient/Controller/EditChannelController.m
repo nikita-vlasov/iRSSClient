e1f8b07a0f8b26f479f5dda9ea7c65381f585eed
Alex <alex.kovalskiy@icloud.com>
2014-04-02 23:40:27 +0300

    Реализованы необходимые методы для реализации добавления/изменения канала.

--- iRSSClient/Controller/EditChannelController.m
+++ iRSSClient/Controller/EditChannelController.m
@@ -1,12 +1,14 @@
 #import "EditChannelController.h"
 #import "EditChannelView.h"
+#import "TextFieldCell.h"
 
-@interface EditChannelController ()
+@interface EditChannelController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
 
 /** View */
 @property (strong, nonatomic) IBOutlet EditChannelView *editChannelView;
 
 @end
+
 
 @implementation EditChannelController
 
@@ -17,6 +19,46 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    TextFieldCell *textFieldCell = [tableView dequeueReusableCellWithIdentifier:@"TextFieldCell"];
+
+    if ([indexPath row] == 0) {
+        [[textFieldCell textField] setPlaceholder:NSLocalizedString(@"Title", nil)];
+        return textFieldCell;
+    }
+    else if ([indexPath row] == 1) {
+        [[textFieldCell textField] setPlaceholder:NSLocalizedString(@"Link", nil)];
+        return textFieldCell;
+    }
+    else if ([indexPath row] == 2) {
+        [[textFieldCell textField] setPlaceholder:NSLocalizedString(@"Description", nil)];
+        return textFieldCell;
+    }
+
+    return nil;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - UITextFieldDelegate
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSLog(@"--- %@", [textField text]);
+
+    return YES;
 }
 
 @end
