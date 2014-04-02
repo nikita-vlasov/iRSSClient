4cb5dba89fc728f202236c4766835b38c5a8a45e
Alex <alex.kovalskiy@icloud.com>
2014-04-02 23:16:36 +0300

    Добавлены классы.

---
+++ iRSSClient/Controller/EditChannelController.h
@@ -0,0 +1,13 @@
+//
+//  EditChannelController.h
+//  iRSSClient
+//
+//  Created by Alex on 4/2/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditChannelController : UIViewController
+
+@end
