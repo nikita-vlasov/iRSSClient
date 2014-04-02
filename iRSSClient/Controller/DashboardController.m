142229d99df10e6d5d2c49a93ae69082219b81ed
Alex <alex.kovalskiy@icloud.com>
2014-04-02 21:12:11 +0300

    add class DashboardController.

---
+++ iRSSClient/Controller/DashboardController.m
@@ -0,0 +1,38 @@
+//
+//  DashboardController.m
+//  iRSSClient
+//
+//  Created by Alex on 4/2/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "DashboardController.h"
+
+@interface DashboardController ()
+
+@end
+
+@implementation DashboardController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
