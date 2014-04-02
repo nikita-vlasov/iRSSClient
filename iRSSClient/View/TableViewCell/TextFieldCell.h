adbcf2189d4c4b41b2f70779e4ee7dfc97640199
Alex <alex.kovalskiy@icloud.com>
2014-04-02 22:40:35 +0300

    Добавлен класс для реализации кастомной ячейки.

---
+++ iRSSClient/View/TableViewCell/TextFieldCell.h
@@ -0,0 +1,13 @@
+//
+//  TextFieldCell.h
+//  iRSSClient
+//
+//  Created by Alex on 4/2/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextFieldCell : UITableViewCell
+
+@end
---
+++ iRSSClient/View/TableViewCell/TextFieldCell.m
@@ -0,0 +1,29 @@
+//
+//  TextFieldCell.m
+//  iRSSClient
+//
+//  Created by Alex on 4/2/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "TextFieldCell.h"
+
+@implementation TextFieldCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
