0438b2b95455d65e8de0211c5d198a3dc6bd7bf4
Alex <alex.kovalskiy@icloud.com>
2014-04-02 22:22:56 +0300

    add class SettingController.

---
+++ iRSSClient/Controller/SettingController.h
@@ -0,0 +1,13 @@
+//
+//  SettingController.h
+//  iRSSClient
+//
+//  Created by Alex on 4/2/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingController : UIViewController
+
+@end
---
+++ iRSSClient/Controller/SettingController.m
@@ -0,0 +1,38 @@
+//
+//  SettingController.m
+//  iRSSClient
+//
+//  Created by Alex on 4/2/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "SettingController.h"
+
+@interface SettingController ()
+
+@end
+
+@implementation SettingController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
