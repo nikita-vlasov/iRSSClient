e565fc6b12fb9d6e18a557d1bb83540fdbedf8c6
Alex <alex.kovalskiy@icloud.com>
2014-04-02 23:41:39 +0300

    Удалены комментарии.

--- iRSSClient/Controller/RssFeedController.m
+++ iRSSClient/Controller/RssFeedController.m
@@ -1,38 +1,19 @@
-//
-//  RssFeedController.m
-//  iRSSClient
-//
-//  Created by Alex on 4/2/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import "RssFeedController.h"
 
 @interface RssFeedController ()
 
 @end
 
+
 @implementation RssFeedController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
+#pragma mark - UIViewController
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
