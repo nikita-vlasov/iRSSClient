b04c471fd9c16a9e65717555e984a8ddf1edc2fa
Alex <alex.kovalskiy@icloud.com>
2014-04-04 01:05:49 +0300

    Реализовано удаление каналов.

--- iRSSClient/Controller/ChannelController.m
+++ iRSSClient/Controller/ChannelController.m
@@ -6,6 +6,8 @@
 
 @interface ChannelController () <UITableViewDataSource, UITableViewDelegate> {
     @private
+    NSManagedObjectContext *managedObjectContext;
+    
     Channel *channel;
     NSMutableArray *arrayChannel;
 }
@@ -23,9 +25,12 @@
     [super viewDidLoad];
 
     [[self navigationItem] setTitle:NSLocalizedString(@"Channel", nil)];
+    [[self navigationItem] setLeftBarButtonItem:[self editButtonItem]];
 
     channel = [[Channel alloc] init];
     arrayChannel = [[NSMutableArray alloc] init];
+
+    managedObjectContext = [(AppDelegate *) [[UIApplication sharedApplication] delegate] managedObjectContext];
 
     _channelView.barButtonOpenChannel = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(opneChannel:)];
     [[self navigationItem] setRightBarButtonItem:[[self channelView] barButtonOpenChannel]];
@@ -39,6 +44,14 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark - UIViewControllerEditing
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
+    [super setEditing:editing animated:animated];
+
+    if (editing) [[_channelView tableView] setEditing:YES animated:YES];
+    else [[_channelView tableView] setEditing:NO animated:YES];
 }
 
 #pragma mark - 
@@ -74,11 +87,40 @@
     return rindDetailCell;
 }
 
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSManagedObject *managedObject= [arrayChannel objectAtIndex:[indexPath row]];
+        [managedObjectContext deleteObject:managedObject];
+
+        NSError *error = nil;
+        [managedObjectContext save:&error];
+
+        if (!error) {
+            [arrayChannel removeObjectAtIndex:[indexPath row]];
+            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        }
+    }
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
 #pragma mark - UITableViewDelegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     channel = [arrayChannel objectAtIndex:[indexPath row]];
     [self performSegueWithIdentifier:@"OpenRssFeed" sender:nil];
+}
+
+
+
+- (void)tableView:(UITableView *)tableView willBeginEditingRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self setEditing:YES];
+}
+
+- (void)tableView:(UITableView *)tableView didEndEditingRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self setEditing:NO];
 }
 
 #pragma mark - Navigation
