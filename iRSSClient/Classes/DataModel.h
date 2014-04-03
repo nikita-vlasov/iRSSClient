29ea314a4221e45116105adff584400a6b2b98db
Alex <alex.kovalskiy@icloud.com>
2014-04-04 00:12:43 +0300

    Удалены комментарии.

--- iRSSClient/Classes/DataModel.h
+++ iRSSClient/Classes/DataModel.h
@@ -1,11 +1,3 @@
-//
-//  DataModel.h
-//  iRSSClient
-//
-//  Created by Alex on 4/3/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface DataModel : NSObject
--- iRSSClient/Classes/DataModel.m
+++ iRSSClient/Classes/DataModel.m
@@ -1,11 +1,3 @@
-//
-//  DataModel.m
-//  iRSSClient
-//
-//  Created by Alex on 4/3/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import "DataModel.h"
 
 @interface DataModel () {
