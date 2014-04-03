268c29c6f0e2be4a084b4762c0293742a975805b
Alex <alex.kovalskiy@icloud.com>
2014-04-04 01:40:08 +0300

    Заполняю данными в моделе.

--- iRSSClient/Model/LeftModel.h
+++ iRSSClient/Model/LeftModel.h
@@ -10,4 +10,12 @@
 
 @interface LeftModel : NSObject
 
+#pragma mark - NSString
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSString *icon;
+@property (strong, nonatomic) NSString *key;
+
+#pragma mark - Other
++ (NSMutableArray *)createLeftMenu;
+
 @end
--- iRSSClient/Model/LeftModel.m
+++ iRSSClient/Model/LeftModel.m
@@ -10,4 +10,27 @@
 
 @implementation LeftModel
 
++ (NSMutableArray *)createLeftMenu {
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+
+    [array addObject:@{@"key"   : @"Dashboard",
+                       @"title" : NSLocalizedString(@"Dashboard", nil),
+                       @"icon"  : @"icon_home.png"}];
+
+    [array addObject:@{@"key"   : @"Channel",
+                       @"title" : NSLocalizedString(@"Channel", nil),
+                       @"icon"  : @"icon_channel.png"}];
+
+    [array addObject:@{@"key"   : @"Setting",
+                       @"title" : NSLocalizedString(@"Setting", nil),
+                       @"icon"  : @"icon_settings.png"}];
+
+    NSMutableArray *arrayObject = [[NSMutableArray alloc] init];
+    for (LeftModel *leftMenu in array) {
+        [arrayObject addObject:leftMenu];
+    }
+
+    return arrayObject;
+}
+
 @end
