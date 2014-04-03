86eef1437898baf65d3b4dca156f2203f2d9c7cb
Alex <alex.kovalskiy@icloud.com>
2014-04-04 01:16:19 +0300

    Добавлен класс.

---
+++ iRSSClient/Model/LeftModel.h
@@ -0,0 +1,13 @@
+//
+//  LeftModel.h
+//  iRSSClient
+//
+//  Created by Alex on 4/4/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LeftModel : NSObject
+
+@end
---
+++ iRSSClient/Model/LeftModel.m
@@ -0,0 +1,13 @@
+//
+//  LeftModel.m
+//  iRSSClient
+//
+//  Created by Alex on 4/4/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "LeftModel.h"
+
+@implementation LeftModel
+
+@end
