ca48a659df8d4c8d068586ded37da5009919fd54
Alex <alex.kovalskiy@icloud.com>
2014-04-04 00:44:53 +0300

    Удалены комментарии.

--- iRSSClient/View/DashboardView.h
+++ iRSSClient/View/DashboardView.h
@@ -1,11 +1,3 @@
-//
-//  DashboardView.h
-//  iRSSClient
-//
-//  Created by Alex on 4/2/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface DashboardView : UIView
