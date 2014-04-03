2f563dba286105ab5f3fcad7ea8f8f1fcd385623
Alex <alex.kovalskiy@icloud.com>
2014-04-04 00:11:54 +0300

    Удлаены комментарии.

--- iRSSClient/View/TableViewCell/IconViewCell.h
+++ iRSSClient/View/TableViewCell/IconViewCell.h
@@ -1,16 +1,9 @@
-//
-//  IconViewCell.h
-//  iRSSClient
-//
-//  Created by Alex on 4/3/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "TextFieldCell.h"
 
 @interface IconViewCell : TextFieldCell
 
+#pragma mark - UILabel
 @property (weak, nonatomic) IBOutlet UILabel *labelTitle;
 
 @end
--- iRSSClient/View/TableViewCell/IconViewCell.m
+++ iRSSClient/View/TableViewCell/IconViewCell.m
@@ -1,29 +1,15 @@
-//
-//  IconViewCell.m
-//  iRSSClient
-//
-//  Created by Alex on 4/3/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import "IconViewCell.h"
 
 @implementation IconViewCell
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
-{
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
-    }
+    if (self) {}
     return self;
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated
-{
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
