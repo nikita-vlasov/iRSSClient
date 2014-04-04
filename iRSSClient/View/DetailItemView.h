4dc7d67e0ebb7064e8e8838a93d320e27d1cfdb2
Alex <alex.kovalskiy@icloud.com>
2014-04-04 22:16:33 +0300

    Добавлен класс - вид.

---
+++ iRSSClient/View/DetailItemView.h
@@ -0,0 +1,13 @@
+//
+//  DetailItemView.h
+//  iRSSClient
+//
+//  Created by Alex on 4/4/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailItemView : UIView
+
+@end
---
+++ iRSSClient/View/DetailItemView.m
@@ -0,0 +1,31 @@
+//
+//  DetailItemView.m
+//  iRSSClient
+//
+//  Created by Alex on 4/4/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "DetailItemView.h"
+
+@implementation DetailItemView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
