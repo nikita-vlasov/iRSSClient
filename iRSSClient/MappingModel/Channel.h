71464940ead093f0dd9b0906c041b656aede24ca
Alex <alex.kovalskiy@icloud.com>
2014-04-05 00:23:46 +0300

    Добавлены поля.

--- iRSSClient/MappingModel/Channel.h
+++ iRSSClient/MappingModel/Channel.h
@@ -10,5 +10,9 @@
 @property (strong, nonatomic) NSString *link;
 /** Дата создания канала. */
 @property (strong, nonatomic) NSString *createDate;
+/** Прочитана статья 1-Да/0-Нет. */
+@property (nonatomic) Boolean *isRead;
+/** Добавлена в список для чтения 1-ДА/0-Нет. */
+@property (nonatomic) Boolean *isFavorites;
 
 @end
