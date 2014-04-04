8c356ed520806156ed9241508fb18142c29dafac
Alex <alex.kovalskiy@icloud.com>
2014-04-04 22:15:42 +0300

    Добавлен класс.

---
+++ iRSSClient/Controller/DetailItemController.h
@@ -0,0 +1,13 @@
+//
+//  DetailItemController.h
+//  iRSSClient
+//
+//  Created by Alex on 4/4/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailItemController : UIViewController
+
+@end
