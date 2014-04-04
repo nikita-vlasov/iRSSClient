b857467c7ecce0af373e4418fd073f2b6de603bf
Alex <alex.kovalskiy@icloud.com>
2014-04-04 23:11:57 +0300

    Добавлено поле для передачи данных.

--- iRSSClient/Controller/DetailItemController.h
+++ iRSSClient/Controller/DetailItemController.h
@@ -1,13 +1,8 @@
-//
-//  DetailItemController.h
-//  iRSSClient
-//
-//  Created by Alex on 4/4/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
+#import "RSSItem.h"
 
 @interface DetailItemController : UIViewController
+
+@property (strong, nonatomic) RSSItem *rssItems;
 
 @end
--- iRSSClient/Controller/DetailItemController.m
+++ iRSSClient/Controller/DetailItemController.m
@@ -1,11 +1,3 @@
-//
-//  DetailItemController.m
-//  iRSSClient
-//
-//  Created by Alex on 4/4/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import "DetailItemController.h"
 
 @interface DetailItemController ()
@@ -14,25 +6,13 @@
 
 @implementation DetailItemController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
+#pragma mark - UIViewController
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
