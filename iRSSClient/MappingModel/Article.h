f41ff60e8defa691d765a7ab305354c501fd2437
Alex <alex.kovalskiy@icloud.com>
2014-04-06 02:13:38 +0300

    Добавлены поля.

--- iRSSClient/MappingModel/Article.h
+++ iRSSClient/MappingModel/Article.h
@@ -10,4 +10,16 @@
 
 @interface Article : NSObject
 
+@property (strong, nonatomic) NSString *author;
+@property (strong, nonatomic) NSString *comments_feed;
+@property (strong, nonatomic) NSString *comments_link;
+@property (strong, nonatomic) NSString *content;
+@property (strong, nonatomic) NSString *guid;
+@property (strong, nonatomic) NSString *item_description;
+@property (strong, nonatomic) NSString *link;
+@property (strong, nonatomic) NSDate *pub_date;
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSNumber *comments_count;
+
+
 @end
