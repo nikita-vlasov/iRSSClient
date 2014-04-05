1309e8ac3a765a0670bf759bdb6de65851678440
Alex <alex.kovalskiy@icloud.com>
2014-04-05 18:17:32 +0300

    Добавлена категория для реализации фаормата даты.

---
+++ iRSSClient/Category/NSDate+Date.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+Date.h
+//  iRSSClient
+//
+//  Created by Alex on 4/5/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Date)
+
+@end
---
+++ iRSSClient/Category/NSDate+Date.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+Date.m
+//  iRSSClient
+//
+//  Created by Alex on 4/5/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "NSDate+Date.h"
+
+@implementation NSDate (Date)
+
+@end
