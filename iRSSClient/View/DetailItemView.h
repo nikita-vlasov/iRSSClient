d080086bf972cec6d36ed389eff68bb5eb549f74
Alex <alex.kovalskiy@icloud.com>
2014-04-05 16:56:50 +0300

    Объявлены лейблы.

--- iRSSClient/View/DetailItemView.h
+++ iRSSClient/View/DetailItemView.h
@@ -1,5 +1,11 @@
 #import <UIKit/UIKit.h>
+#import "LeftView.h"
 
-@interface DetailItemView : UIView
+@interface DetailItemView : LeftView
+
+#pragma mark - UILabel
+@property (weak, nonatomic) IBOutlet UILabel *labelTitle;
+@property (weak, nonatomic) IBOutlet UILabel *labelDate;
+@property (weak, nonatomic) IBOutlet UILabel *labelAuthor;
 
 @end
