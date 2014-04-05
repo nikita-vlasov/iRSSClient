04b48cd47c9910f214c8c3a07369f8a8f3dd2e2d
Alex <alex.kovalskiy@icloud.com>
2014-04-05 19:35:17 +0300

    Реализовано открытие актион шита.

--- iRSSClient/Controller/DetailItemController.m
+++ iRSSClient/Controller/DetailItemController.m
@@ -3,7 +3,10 @@
 #import "TextViewCell.h"
 #import "NSDate+Date.h"
 
-@interface DetailItemController () <UITableViewDataSource, UITableViewDelegate>
+@interface DetailItemController () <UITableViewDataSource, UITableViewDelegate, UIActionSheetDelegate> {
+    @private
+    UIBarButtonItem *barButtonShare;
+}
 
 /** View. */
 @property (strong, nonatomic) IBOutlet DetailItemView *detailItemView;
@@ -16,6 +19,20 @@
 #pragma mark - UIViewController
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    barButtonShare = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction
+                                                                   target:self
+                                                                   action:@selector(shareButton:)];
+    [[self navigationItem] setRightBarButtonItems:@[barButtonShare]];
+
+    [[[self detailItemView] actionSheetShare] addButtonWithTitle:NSLocalizedString(@"E-Mail", nil)];
+    [[[self detailItemView] actionSheetShare] addButtonWithTitle:NSLocalizedString(@"Message", nil)];
+    [[[self detailItemView] actionSheetShare] addButtonWithTitle:NSLocalizedString(@"Facebook", nil)];
+    [[[self detailItemView] actionSheetShare] addButtonWithTitle:NSLocalizedString(@"Twitter", nil)];
+    [[[self detailItemView] actionSheetShare] addButtonWithTitle:NSLocalizedString(@"Cancel", nil)];
+
+    [[[self detailItemView] actionSheetShare] setCancelButtonIndex:4];
+    [[[self detailItemView] actionSheetShare] setDelegate:self];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -32,6 +49,11 @@
     [[[self detailItemView] labelTitle] setText:[[self rssItems] title]];
     [[[self detailItemView] labelAuthor] setText:[[self rssItems] author]];
     [[[self detailItemView] labelDate] setText:[NSDate getFormattetDateSetDate:[[self rssItems] pubDate]]];
+}
+
+#pragma mark - UIBarButtonItem
+- (void)shareButton:(UIBarButtonItem *)sender {
+    [[[self detailItemView] actionSheetShare] showInView:[self detailItemView]];
 }
 
 #pragma mark - UITableViewDataSource
@@ -51,6 +73,7 @@
     return textViewCell;
 }
 
+#pragma mark - UITableViewDelegate
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITextView *textView = [[UITextView alloc] init];
     NSAttributedString *string = [[NSAttributedString alloc] initWithString:[[self rssItems] itemDescription]];
@@ -60,6 +83,36 @@
     CGSize size = [textView sizeThatFits:CGSizeMake(300.0f, FLT_MAX)];
 
     return size.height;
+
+}
+
+#pragma mark - UIActionSheetDelegate
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if ([[self detailItemView] actionSheetShare] == actionSheet) {
+        switch (buttonIndex) {
+            case 0: [self shareEMail]; break;
+            case 1: [self shareMessage]; break;
+            case 2: [self shareFacebook]; break;
+            case 3: [self shareTwitter]; break;
+            default: break;
+        }
+    }
+}
+
+#pragma mark - Share
+- (void)shareEMail {
+
+}
+
+- (void)shareMessage {
+
+}
+
+- (void)shareFacebook {
+
+}
+
+- (void)shareTwitter {
 
 }
 
