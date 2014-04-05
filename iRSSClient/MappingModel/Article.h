ef638452a3aaedbe45b2d6999aad49a81ae1520d
Alex <alex.kovalskiy@icloud.com>
2014-04-06 02:13:52 +0300

    Удалены комментарии.

--- iRSSClient/MappingModel/Article.h
+++ iRSSClient/MappingModel/Article.h
@@ -1,11 +1,3 @@
-//
-//  Article.h
-//  iRSSClient
-//
-//  Created by Alex on 4/6/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface Article : NSObject
--- iRSSClient/MappingModel/Article.m
+++ iRSSClient/MappingModel/Article.m
@@ -1,11 +1,3 @@
-//
-//  Article.m
-//  iRSSClient
-//
-//  Created by Alex on 4/6/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import "Article.h"
 
 @implementation Article
