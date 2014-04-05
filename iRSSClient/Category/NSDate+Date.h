1d6d6be0699426df0f436fb8b3b8f299f3549642
Alex <alex.kovalskiy@icloud.com>
2014-04-05 18:17:45 +0300

    Удалены комментарии.

--- iRSSClient/Category/NSDate+Date.h
+++ iRSSClient/Category/NSDate+Date.h
@@ -1,11 +1,3 @@
-//
-//  NSDate+Date.h
-//  iRSSClient
-//
-//  Created by Alex on 4/5/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface NSDate (Date)
--- iRSSClient/Category/NSDate+Date.m
+++ iRSSClient/Category/NSDate+Date.m
@@ -1,11 +1,3 @@
-//
-//  NSDate+Date.m
-//  iRSSClient
-//
-//  Created by Alex on 4/5/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import "NSDate+Date.h"
 
 @implementation NSDate (Date)
