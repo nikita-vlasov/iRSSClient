a08bb5b83ce70b585d996ed6554cceafec6ab4ea
Alex <alex.kovalskiy@icloud.com>
2014-04-05 18:36:31 +0300

    Изменен метод преобразования даты.

--- iRSSClient/Category/NSDate+Date.m
+++ iRSSClient/Category/NSDate+Date.m
@@ -2,4 +2,12 @@
 
 @implementation NSDate (Date)
 
++ (NSString *)getFormattetDateSetDate:(NSDate *)date {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy/MM/dd HH:mm"];
+    NSString *formatterDate = [dateFormatter stringFromDate:date];
+
+    return formatterDate;
+}
+
 @end
