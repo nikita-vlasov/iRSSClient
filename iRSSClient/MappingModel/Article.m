9ad6b25c42492dcc8f2cdd1c0b1b082df71c6e07
Alex <alex.kovalskiy@icloud.com>
2014-04-06 02:11:03 +0300

    Добавлен класс.

---
+++ iRSSClient/MappingModel/Article.m
@@ -0,0 +1,13 @@
+//
+//  Article.m
+//  iRSSClient
+//
+//  Created by Alex on 4/6/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "Article.h"
+
+@implementation Article
+
+@end
