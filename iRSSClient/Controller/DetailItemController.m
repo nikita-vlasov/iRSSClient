b22ec0d9fd5d0701ac30179aeb809e0636efff98
Alex <alex.kovalskiy@icloud.com>
2014-04-05 19:59:24 +0300

    + Шаринг.

--- iRSSClient/Controller/DetailItemController.m
+++ iRSSClient/Controller/DetailItemController.m
@@ -2,10 +2,23 @@
 #import "DetailItemView.h"
 #import "TextViewCell.h"
 #import "NSDate+Date.h"
+#import <Social/Social.h>
+#import <MessageUI/MFMailComposeViewController.h>
+#import <MessageUI/MessageUI.h>
+#import "SVWebViewController.h"
 
-@interface DetailItemController () <UITableViewDataSource, UITableViewDelegate, UIActionSheetDelegate> {
+@interface DetailItemController () <UITableViewDataSource, UITableViewDelegate, UIActionSheetDelegate, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate> {
     @private
     UIBarButtonItem *barButtonShare;
+
+    /** Для отправки информации о коде через E-Mail. */
+    MFMailComposeViewController *mfMailComposeViewController;
+    /** Для отправки информации о коде через Message. */
+    MFMessageComposeViewController *mfMessageComposeViewController;
+    /** Шаринг в соц.сети. */
+    SLComposeViewController *slComposeViewController;
+    /** WebViewController */
+    SVModalWebViewController *modalWebViewController;
 }
 
 /** View. */
@@ -29,9 +42,10 @@
     [[[self detailItemView] actionSheetShare] addButtonWithTitle:NSLocalizedString(@"Message", nil)];
     [[[self detailItemView] actionSheetShare] addButtonWithTitle:NSLocalizedString(@"Facebook", nil)];
     [[[self detailItemView] actionSheetShare] addButtonWithTitle:NSLocalizedString(@"Twitter", nil)];
+    [[[self detailItemView] actionSheetShare] addButtonWithTitle:NSLocalizedString(@"Open Link", nil)];
     [[[self detailItemView] actionSheetShare] addButtonWithTitle:NSLocalizedString(@"Cancel", nil)];
 
-    [[[self detailItemView] actionSheetShare] setCancelButtonIndex:4];
+    [[[self detailItemView] actionSheetShare] setCancelButtonIndex:5];
     [[[self detailItemView] actionSheetShare] setDelegate:self];
 }
 
@@ -43,6 +57,12 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+- (void)dealloc {
+    mfMailComposeViewController = nil;
+    slComposeViewController = nil;
+    modalWebViewController = nil;
 }
 
 - (void)reloadData {
@@ -94,26 +114,81 @@
             case 1: [self shareMessage]; break;
             case 2: [self shareFacebook]; break;
             case 3: [self shareTwitter]; break;
+            case 4: [self openWebView]; break;
             default: break;
         }
     }
 }
 
-#pragma mark - Share
-- (void)shareEMail {
-
+#pragma mark - MFMailComposeViewControllerDelegate
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error; {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (void)shareMessage {
-
+#pragma mark - MFMessageComposeViewControllerDelegate
+- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+#pragma mark - Share Method
 - (void)shareFacebook {
+    @try {
+        slComposeViewController = [[SLComposeViewController alloc] init];
+        slComposeViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        [slComposeViewController setInitialText:[[self rssItems] itemDescription]];
+        [slComposeViewController addURL:[[self rssItems] link]];
 
+        [self presentViewController:slComposeViewController animated:YES completion:nil];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Ошибка при ширинге Facebook: %@", exception);
+    }
 }
 
 - (void)shareTwitter {
+    @try {
+        slComposeViewController = [[SLComposeViewController alloc] init];
+        slComposeViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+        [slComposeViewController setInitialText:[[self rssItems] itemDescription]];
+        [slComposeViewController addURL:[[self rssItems] link]];
 
+        [self presentViewController:slComposeViewController animated:YES completion:nil];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Ошибка при ширинге в Twitter: %@", exception);
+    }
+}
+
+- (void)shareEMail {
+    @try {
+        mfMailComposeViewController = [[MFMailComposeViewController alloc] init];
+        [mfMailComposeViewController setSubject:[[self rssItems] title]];
+        [mfMailComposeViewController setMessageBody:[[self rssItems] itemDescription] isHTML:YES];
+        [mfMailComposeViewController setMailComposeDelegate:self];
+
+        [self presentViewController:mfMailComposeViewController animated:YES completion:nil];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Ошибка при открытии E-Mail контроллера: %@", exception);
+    }
+}
+
+- (void)shareMessage {
+    @try {
+        mfMessageComposeViewController = [[MFMessageComposeViewController alloc] init];
+        [mfMessageComposeViewController setBody:[[self rssItems] itemDescription]];
+        [mfMessageComposeViewController setMessageComposeDelegate:self];
+
+        [self presentViewController:mfMessageComposeViewController animated:YES completion:nil];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Ошибка при открытии SMS контроллера: %@", exception);
+    }
+}
+
+- (void)openWebView {
+    modalWebViewController = [[SVModalWebViewController alloc] initWithURL:[[self rssItems] link]];
+    [self presentViewController:modalWebViewController animated:YES completion:nil];
 }
 
 @end
