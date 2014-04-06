ff050fe003572d1151a90cf2739c906e9d34839e
Alex <alex.kovalskiy@icloud.com>
2014-04-06 17:55:16 +0300

    Удалены комментарии.

--- iRSSClient/View/FontSizeView.h
+++ iRSSClient/View/FontSizeView.h
@@ -1,11 +1,3 @@
-//
-//  FontSizeView.h
-//  iRSSClient
-//
-//  Created by Alex on 4/6/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface FontSizeView : UIView
--- iRSSClient/View/FontSizeView.m
+++ iRSSClient/View/FontSizeView.m
@@ -1,21 +1,10 @@
-//
-//  FontSizeView.m
-//  iRSSClient
-//
-//  Created by Alex on 4/6/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import "FontSizeView.h"
 
 @implementation FontSizeView
 
-- (id)initWithFrame:(CGRect)frame
-{
+- (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
+    if (self) {}
     return self;
 }
 
