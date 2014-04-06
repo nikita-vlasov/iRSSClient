5a6aece4aa9a6aeec8536e05d80fa8878b572a6a
Alex <alex.kovalskiy@icloud.com>
2014-04-06 17:54:11 +0300

    Добавлен клас - контролер.

---
+++ iRSSClient/Controller/FontSizeController.h
@@ -0,0 +1,13 @@
+//
+//  FontSizeController.h
+//  iRSSClient
+//
+//  Created by Alex on 4/6/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FontSizeController : UIViewController
+
+@end
