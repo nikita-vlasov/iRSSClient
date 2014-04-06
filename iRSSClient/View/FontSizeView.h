778ae2607476b232e1513de2e026631429cdc5ba
Alex <alex.kovalskiy@icloud.com>
2014-04-06 21:54:10 +0300

    Объявлен лейбл.

--- iRSSClient/View/FontSizeView.h
+++ iRSSClient/View/FontSizeView.h
@@ -5,6 +5,9 @@
 #pragma mark - UIStepper
 @property (weak, nonatomic) IBOutlet UIStepper *stepperButton;
 
+#pragma mark - UILabel
+@property (weak, nonatomic) IBOutlet UILabel *labelFontSize;
+
 #pragma mark - UINavigationBar
 @property (weak, nonatomic) IBOutlet UINavigationBar *navigationBar;
 
