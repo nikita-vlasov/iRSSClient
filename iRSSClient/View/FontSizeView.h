e99ae51a396df8755da193f7759fab4011ca8666
Alex <alex.kovalskiy@icloud.com>
2014-04-06 17:53:56 +0300

    Добавлен вид.

---
+++ iRSSClient/View/FontSizeView.h
@@ -0,0 +1,13 @@
+//
+//  FontSizeView.h
+//  iRSSClient
+//
+//  Created by Alex on 4/6/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FontSizeView : UIView
+
+@end
---
+++ iRSSClient/View/FontSizeView.m
@@ -0,0 +1,31 @@
+//
+//  FontSizeView.m
+//  iRSSClient
+//
+//  Created by Alex on 4/6/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "FontSizeView.h"
+
+@implementation FontSizeView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
