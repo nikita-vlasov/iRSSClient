a21340cb20167c4c5d95bc5a735df3831ae88638
Alex <alex.kovalskiy@icloud.com>
2014-04-06 17:55:20 +0300

    Удалены комментарии.

--- iRSSClient/Controller/FontSizeController.h
+++ iRSSClient/Controller/FontSizeController.h
@@ -1,11 +1,3 @@
-//
-//  FontSizeController.h
-//  iRSSClient
-//
-//  Created by Alex on 4/6/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface FontSizeController : UIViewController
