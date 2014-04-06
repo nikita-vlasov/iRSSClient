8558e9ec8bc34d3d637dd35cabd64dcb3f451361
Alex <alex.kovalskiy@icloud.com>
2014-04-06 22:04:39 +0300

    Задан заголовок.

--- iRSSClient/Controller/FontSizeController.m
+++ iRSSClient/Controller/FontSizeController.m
@@ -15,6 +15,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    [[[[self fontSizeView] navigationBar] topItem] setTitle:NSLocalizedString(@"Font size", nil)];
+
     [[[self fontSizeView] stepperButton] addTarget:self action:@selector(stepperPressed:) forControlEvents:UIControlEventValueChanged];
     [[[self fontSizeView] stepperButton] setValue:[[NSUserDefaults standardUserDefaults] doubleForKey:@"content_font_size"]];
 
@@ -22,6 +24,8 @@
     [[[self fontSizeView] stepperButton] setMinimumValue:10.0f];
     /* Устанавливаю максимальное значение. */
     [[[self fontSizeView] stepperButton] setMaximumValue:30.0f];
+
+    [[[self fontSizeView] labelFontSize] setFont:[UIFont fontWithName:@"Helvetica Neue" size:[[[self fontSizeView] stepperButton] value]]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,6 +35,8 @@
 #pragma mark - Button Pressed
 - (void)stepperPressed:(UIStepper *)sender {
     NSLog(@"%f", [sender value]);
+
+    [[[self fontSizeView] labelFontSize] setFont:[UIFont fontWithName:@"Helvetica Neue" size:[sender value]]];
 
     [[NSUserDefaults standardUserDefaults] setDouble:[sender value] forKey:@"content_font_size"];
     [[NSUserDefaults standardUserDefaults] synchronize];
